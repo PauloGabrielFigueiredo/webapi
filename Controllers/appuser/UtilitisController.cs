4639261ff77b4ed6dc7cc12380b68242c2a1bdcc
Paulo Gabriel Figueiredo <a37555@alunos.ipb.pt>
2022-07-06 15:27:27 +0100

    update

--- Controllers/appuser/UtilitisController.cs
+++ Controllers/appuser/UtilitisController.cs
@@ -12,7 +12,22 @@ namespace webapi.Controllers.appuser
     {
         return Ok("Version 1.0");
 
+
         
     }
-    }
+[HttpGet, Route("content/{category}")]
+    public ActionResult<(ContentResult, string)> ConfirmVerify(string category)
+    {   
+        var html="";
+        try{
+        html = System.IO.File.ReadAllText(@"./assests/html/" + category + ".html");
+    } 
+    catch(SystemException){ return NotFound("File not found");
+    } 
+        return Ok((base.Content(html, "text/html"), category));
+    }}
+    
+
+
+
 }
